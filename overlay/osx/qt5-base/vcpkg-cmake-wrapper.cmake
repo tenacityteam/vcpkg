fba5744e479832e87c7da0aafb11d72a47ef1f76
Daniel Schürmann <daschuer@mixxx.org>
2021-07-11 11:04:58 +0200

    [qt5-base] use PostgreSQL::PostgreSQL

--- overlay/osx/qt5-base/vcpkg-cmake-wrapper.cmake
+++ overlay/osx/qt5-base/vcpkg-cmake-wrapper.cmake
@@ -23,12 +23,11 @@ if("${_target_type}" STREQUAL "STATIC_LIBRARY")
 
     set_property(TARGET Qt5::Core APPEND PROPERTY INTERFACE_LINK_LIBRARIES
         ZLIB::ZLIB JPEG::JPEG PNG::PNG Freetype::Freetype SQLite3::SQLite3 harfbuzz::harfbuzz
-        double-conversion::double-conversion OpenSSL::SSL OpenSSL::Crypto
+        double-conversion::double-conversion PostgreSQL::PostgreSQL OpenSSL::SSL OpenSSL::Crypto
     )
 
     add_qt_library(Qt5::Core
         pcre2-16
-        libpq
         Qt5ThemeSupport
         Qt5EventDispatcherSupport
         Qt5PlatformCompositorSupport
