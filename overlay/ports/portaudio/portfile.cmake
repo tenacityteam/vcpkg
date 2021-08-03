f52e59bfe969f97def536827267dc36956afbc2b
Be <be@mixxx.org>
2021-08-03 13:59:55 -0500

    [portaudio] add support for JACK on Windows and macOS

--- overlay/ports/portaudio/portfile.cmake
+++ overlay/ports/portaudio/portfile.cmake
@@ -2,12 +2,13 @@ vcpkg_fail_port_install(ON_TARGET "uwp")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Be-ing/portaudio
-    REF 62455aa6b4791f714ab8c2f2857af54f93760115
-    SHA512 5864e4d36e2532fce1f79bfbbe6fef5304d45cf647e8413761f1b69cc3c7dbe87758c3c4a9a8d34290db92ebe6ca055755460b2db528cb8491cbe22b28304ca0
-    HEAD_REF cmake_rewrite
+    REF 3364bca3c01a0aedf64ba5a5abeb8e7019717ffc
+    SHA512 ce67f3e32cd55c02f4966735b39e329016a6723de3e8eedde87254df6b22a4eaa7f2fcc99729125a1baff52dea35cb4664a3dec26805fe75425a167371963aaf
+    HEAD_REF jack_windows
 )
 
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS FEATURES
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  FEATURES
     asio ASIO
     jack JACK
 )
@@ -21,16 +22,12 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-endif()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
