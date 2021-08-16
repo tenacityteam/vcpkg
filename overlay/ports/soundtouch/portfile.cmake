d6d886f83c4ba86dc657ddb6ae395cb9f34ea0b5
Be <be@mixxx.org>
2021-08-15 21:58:17 -0500

    [soundtouch] build my fork of 2.2 with CMake

--- overlay/ports/soundtouch/portfile.cmake
+++ overlay/ports/soundtouch/portfile.cmake
@@ -1,21 +1,20 @@
 vcpkg_fail_port_install(ON_TARGET "UWP")
 
 vcpkg_from_gitlab(
-    GITLAB_URL https://gitlab.com/
-    REPO soundtouch/soundtouch
-    REF 2.2
-    SHA512 a84b652867a4549cedc304956bd8e3ff2272992adbbcb42405d8da3aefd1a352cb651aecce3abff783c816793ea72ddb7c51164db4d0c4cd64637f8e885b9f7d
+    GITLAB_URL https://gitlab.com
+    REPO Be_/soundtouch
+    REF 3617bd166b7ed3f3a94b30f7412f79c9ad792cf9
+    SHA512 c0d8cf9c4d372746ed8709385df48418380e58585caace03f7c8a1a9d11381d3df98b433416427fb48a7b204ee3cde5f1970ad1a165bb64277db7287eed8b1ac
     OUT_SOURCE_PATH SOURCE_PATH
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_install()
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/SoundTouch)
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
 
-vcpkg_install_cmake()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(INSTALL ${SOURCE_PATH}/COPYING.TXT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
