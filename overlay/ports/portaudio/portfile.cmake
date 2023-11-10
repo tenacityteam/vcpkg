8b7bc931d92e5b71207e8a6b57af9c28125d0fb9
Avery King <avery98@pm.me>
2023-11-09 21:19:55 -0800

    PortAudio: Rebase git port on latest upstream port
    Signed-off-by: Avery King <avery98@pm.me>

--- overlay/ports/portaudio/portfile.cmake
+++ overlay/ports/portaudio/portfile.cmake
@@ -4,29 +4,43 @@ vcpkg_from_github(
     REF 95a5c4ba645e01b32f70458f8ddcd92edd62f982
     SHA512 8ed8042e38e1c9a2ff8f9e4b72bcc760ffb80b53b29542c40f47be242955bcff897f93272847931332585c3660d9ab3ddce4cb86a9fccc21183d3312bd2d660f
     HEAD_REF master
+    PATCHES
+        fix-library-can-not-be-found.patch
+        framework_link.patch
 )
 
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-  FEATURES
-    asio PA_USE_ASIO
-    jack PA_USE_JACK
-)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" PA_BUILD_SHARED)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" PA_BUILD_STATIC)
 
+# NOTE: the ASIO backend will be built automatically if the ASIO-SDK is provided
+# in a sibling folder of the portaudio source in vcpkg/buildtrees/portaudio/src
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS ${FEATURE_OPTIONS}
-    # The ASIO variable is only used on Windows.
-    MAYBE_UNUSED_VARIABLES PA_USE_ASIO
-    OPTIONS_DEBUG -DPA_ENABLE_DEBUG_OUTPUT:BOOL=ON
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DPA_USE_DS=ON
+        -DPA_USE_WASAPI=ON
+        -DPA_USE_WDMKS=ON
+        -DPA_USE_WMME=ON
+        -DPA_LIBNAME_ADD_SUFFIX=OFF
+        -DPA_BUILD_SHARED=${PA_BUILD_SHARED}
+        -DPA_BUILD_STATIC=${PA_BUILD_STATIC}
+        -DPA_DLL_LINK_WITH_STATIC_RUNTIME=OFF
+    OPTIONS_DEBUG
+        -DPA_ENABLE_DEBUG_OUTPUT:BOOL=ON
 )
-vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
-vcpkg_fixup_pkgconfig()
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
 # Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+vcpkg_fixup_pkgconfig()
