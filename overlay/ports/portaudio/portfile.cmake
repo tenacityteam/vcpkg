96509dcca2d866c8f582f745bdb8639561e54e5f
Avery King <avery98@pm.me>
2023-11-10 20:12:05 -0800

    PortAudio: Remove patches
    Signed-off-by: Avery King <avery98@pm.me>

--- overlay/ports/portaudio/portfile.cmake
+++ overlay/ports/portaudio/portfile.cmake
@@ -4,9 +4,6 @@ vcpkg_from_github(
     REF 95a5c4ba645e01b32f70458f8ddcd92edd62f982
     SHA512 8ed8042e38e1c9a2ff8f9e4b72bcc760ffb80b53b29542c40f47be242955bcff897f93272847931332585c3660d9ab3ddce4cb86a9fccc21183d3312bd2d660f
     HEAD_REF master
-    PATCHES
-        fix-library-can-not-be-found.patch
-        framework_link.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" PA_BUILD_SHARED)
